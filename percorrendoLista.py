b2317bc47826e29cb9921df4a1b4e5464e45c06c
Danubiabuba <danubiaalves184@gmail.com>
2024-03-19 13:57:59 -0300

    Percorrendo lista

---
+++ percorrendoLista.py
@@ -0,0 +1,4 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+print("Quantidade de elementos é",len(lista_musicos))
+print("")
+print("Dado salvo no indice 2 -",lista_musicos[2]+"\n","Dado salvo no indice 9-",lista_musicos[9]+"\n", "Dado salvo no indice 14-",lista_musicos[14])
