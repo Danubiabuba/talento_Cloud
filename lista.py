a4add6feac8c18fa34aaa6d3803c2f5a1d8966f9
Danubiabuba <danubiaalves184@gmail.com>
2024-03-19 12:42:39 -0300

    Utilizando Array

---
+++ lista.py
@@ -0,0 +1,9 @@
+# Array de produtos da loja de roupas.
+produtos_moda = ["Vestidos", "Blusas", "Calças", "Macacão", "Shorts"]
+
+#Array de anos de nascimentos de familiares e amigos.
+anos_nasc = [1989, 1982, 1988, 1960, 1952, 2014]
+
+print("Produtos da loja de roupas:", produtos_moda)
+print("")
+print("Anos de nascimento de amigos e familiares:", anos_nasc)
