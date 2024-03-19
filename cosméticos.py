1788d56c8ee811400abb2667ef107c3d278d0d7a
Danubiabuba <danubiaalves184@gmail.com>
2024-03-19 15:08:34 -0300

    correção do lista cosméticos

--- cosméticos.py
+++ cosméticos.py
@@ -1,4 +1,12 @@
-lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
-for produto in lista_produtos:
-    print("Temos {} á venda!".format(produto))
+#lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
+#or produto in lista_produtos:
+    #print("Temos {} á venda!".format(produto))#
 
+#Imprimir cada um dos itens da listas.
+lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
+for i in range(len(lista_produtos)):
+    print(lista_produtos)
+
+#Concatenar os itens da lista coma a frase.
+for i in range(len(lista_produtos)):
+    print("Temos"  , lista_produtos[i] , "á venda!")
