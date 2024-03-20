ef701a38764e10746283c6fd0a4a49f673a525a8
Danubiabuba <danubiaalves184@gmail.com>
2024-03-20 08:54:39 -0300

    Manipulando array add e removendo indice

--- cosméticos.py
+++ cosméticos.py
@@ -1,12 +1,19 @@
-#lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
-#or produto in lista_produtos:
-    #print("Temos {} á venda!".format(produto))#
+# lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
+# or produto in lista_produtos:
+# print("Temos {} á venda!".format(produto))#
 
-#Imprimir cada um dos itens da listas.
-lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","xampus","sabonetes"," delineadores"]
+# Imprimir cada um dos itens da listas.
+lista_produtos = ["máscaras faciais","batons","esmaltes","perfumes","loções","shampoo","sabonetes"," delineadores"]
 for i in range(len(lista_produtos)):
     print(lista_produtos)
 
-#Concatenar os itens da lista coma a frase.
+# Sustituindo dois indices a lista
+lista_produtos[1] = "Rímel"
+lista_produtos [4] = "Creme hidratante"
+
+# Removendo dois indices da lista
+lista_produtos.pop(7)
+
+# Concatenar os itens da lista coma a frase.
 for i in range(len(lista_produtos)):
     print("Temos"  , lista_produtos[i] , "á venda!")
